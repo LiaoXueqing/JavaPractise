92b7032baa9ca1a2f50ff89986407ead496cbfbe
xuexue <1047504737@qq.com>
2018-04-10 23:07:16 +0800

    section3 practiceC

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,5 +1,6 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,23 @@ public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+        Map<String, Integer> collection3 = new HashMap<String, Integer>();
+        for(String s : collectionA){
+            if(collection3.get(s)==null){
+                collection3.put(s,1);
+            }else{
+                collection3.put(s,collection3.get(s)+1);
+            }
+        }
+
+        List<String> listB = object.get("value");
+        for(String s1 : listB){
+            if(collection3.containsKey(s1)){
+                if(collection3.get(s1)>=3) {
+                    collection3.put(s1, collection3.get(s1) - collection3.get(s1)/3);
+                }
+            }
+        }
+        return collection3;
     }
 }
