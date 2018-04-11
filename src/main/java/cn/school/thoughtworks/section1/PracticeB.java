385746c3d935cbf6e234894750ae9c19c668a595
xuexue <1047504737@qq.com>
2018-04-11 14:07:52 +0800

    update section1 practiceB use lambda

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -7,15 +7,6 @@ import java.util.stream.Collectors;
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        List<String> resultList = new ArrayList<>();
-        List<String> collection3 = collection2.get(0);
-        for(String s1:collection1){
-            for(String s3:collection3){
-                if(s3.equals(s1)){
-                    resultList.add(s1);
-                }
-            }
-        }
-        return resultList;
+        return collection1.stream().filter(s1->collection2.get(0).contains(s1)).collect(Collectors.toList());
     }
 }
